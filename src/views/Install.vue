69a7a8d30e7e941b3c07b0bc093cf9fbe162f875
yzzting <yzz1995@aliyun.com>
2016-06-28 17:58:17 +0800

    add install

---
+++ src/views/Install.vue
@@ -0,0 +1,28 @@
+<template>
+    <header>
+        <nav class="nav-header">
+            <a v-link="{path:'/install'}" class="header-button">Install</a>
+        </nav>
+    </header>
+    <div class="container">
+        <h2>安装</h2>
+        <p>
+            通过npm安装
+        </p>
+        <div class="code-body" lang="shell">
+            <pre>
+                <code>
+                    npm install --save yzzmdl
+                </code>
+            </pre>
+        </div>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
