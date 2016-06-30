f3ea74390a3929748f0462d6c8f4b6549a0603d8
yzzting <yzz1995@aliyun.com>
2016-06-30 11:48:42 +0800

    adjust router structure

--- src/entry.js
+++ src/entry.js
@@ -1,49 +1,9 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './app.vue';
-import Views from './views/index.js';
+
 import markdown from './doc/mark.js';
-Vue.use(VueRouter);
-var router = new VueRouter();
-router.map({
-    '/about':{
-        component:Views.About
-    },
-    '/buttons': {
-        component:Views.Buttons
-    },
-    '/cards': {
-        component:Views.Cards
-    },
-    '/chips': {
-        component:Views.Chips
-    },
-    '/footer':{
-        component:Views.Footer
-    },
-    '/forms':{
-        component:Views.Forms
-    },
-    '/navbar':{
-        component:Views.Navbar
-    },
-    '/preloader':{
-        component:Views.Preloader
-    },
-    '/start':{
-        component:Views.Start
-    },
-    '/install':{
-        component:Views.Install
-    }
-});
-router.beforeEach(function () {
-  window.scrollTo(0, 0)
-})
+import router from './router';
 
-router.redirect({
-  '*': '/about'
-})
 router.start(App, '#app');
-
 markdown.markdown();
---
+++ src/router.js
@@ -0,0 +1,48 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import Views from './views/index.js';
+Vue.use(VueRouter);
+var router = new VueRouter();
+router.map({
+    '/about':{
+        component:Views.About
+    },
+    '/buttons': {
+        component:Views.Buttons
+    },
+    '/cards': {
+        component:Views.Cards
+    },
+    '/chips': {
+        component:Views.Chips
+    },
+    '/footer':{
+        component:Views.Footer
+    },
+    '/forms':{
+        component:Views.Forms
+    },
+    '/navbar':{
+        component:Views.Navbar
+    },
+    '/preloader':{
+        component:Views.Preloader
+    },
+    '/start':{
+        component:Views.Start
+    },
+    '/install':{
+        component:Views.Install
+    }
+});
+router.beforeEach(function () {
+  window.scrollTo(0, 0)
+})
+
+router.redirect({
+  '*': '/about'
+})
+
+
+
+export default router
